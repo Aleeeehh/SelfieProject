0ed982053d8eccfde8245837162a9208e2a73726
AndreaS@pc <andrea.santilli.jr@gmail.com>
2024-10-27 19:28:06 +0100

    improved login's page with images and css

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export default function Login(): React.JSX.Element {
 	const [username, setUsername] = React.useState("");
 	const [password, setPassword] = React.useState("");
 	const [clearPswd, setClearPswd] = React.useState(false);
 	const [message, setMessage] = React.useState("");
+	const [errorMessage, setErrorMessage] = React.useState("");
 
 	const nav = useNavigate();
 	const { login, isLoggedIn } = useAuth();
@@ -25,7 +28,7 @@ export default function Login(): React.JSX.Element {
 				setMessage("Utente authenticato");
 				nav("/");
 			} else {
-				setMessage("Credenziali errate");
+				setErrorMessage("Credenziali errate");
 			}
 		} catch (e) {
 			setMessage("Impossibile raggiungere il server");
@@ -33,39 +36,51 @@ export default function Login(): React.JSX.Element {
 	}
 
 	return (
-		<div className="login-form">
-			<form>
-				<label htmlFor="username">
-					Username
-					<input
-						type="text"
-						name="username"
-						value={username}
-						onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
-							setUsername(e.target.value);
-						}}
-					/>
-				</label>
-				<label htmlFor="password">
-					Password
-					<input
-						type={clearPswd ? "text" : "password"}
-						name="password"
-						value={password}
-						onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-							setPassword(e.target.value)
-						}
-					/>
-				</label>
-				<input
-					type="checkbox"
-					checked={clearPswd}
-					onClick={(): void => setClearPswd(!clearPswd)}
-				/>
-				<button onClick={handleLogin}>Login</button>
-			</form>
-
+		<div className="body">
 			{message && <div>{message}</div>}
+			<div className="background">
+				<div className="login-container">
+					<div className="avatar">
+						<img src="/images/avatar.png" alt="Avatar" />
+					</div>
+					<form>
+						<label htmlFor="username">
+							Username: 
+							<input
+								type="text"
+								name="username"
+								value={username}
+								onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
+									setUsername(e.target.value);
+								}}
+							/>
+						</label>
+
+						<label htmlFor="password">
+							Password: 
+							<input
+								type={clearPswd ? "text" : "password"}
+								name="password"
+								value={password}
+								onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+									setPassword(e.target.value)
+								}
+							/>
+							Nascondi: 
+							<i
+								className={`bi ${clearPswd ? "bi-eye" : "bi-eye-slash"}`}
+								onClick={(): void => setClearPswd(!clearPswd)}
+								style={{ cursor: "pointer", marginLeft: "8px" }}
+							></i>
+						</label>
+
+						<button onClick={handleLogin}>Login</button>
+						<p className="error-message">{errorMessage}</p>
+						<p className="register-link">Not yet registered? <a href="/register">Click here!</a></p>
+						
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 }
