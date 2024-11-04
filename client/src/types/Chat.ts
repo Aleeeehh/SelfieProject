d357983675ce34806df8ea41c16452130708509a
leonardopinna <96293968+leonardopinna@users.noreply.github.com>
2024-11-04 17:02:12 +0100

    Added projects design doc. Fixed bug on client types.

--- client/src/types/Chat.ts
+++ client/src/types/Chat.ts
@@ -1,12 +1,11 @@
-import { Types } from "mongoose";
-import UserResult from "./UserResult.ts";
-import Message from "./Message.ts";
+import UserResult from "./UserResult.js";
+import Message from "./Message.js";
 
 type Chat = {
-    id?: string | Types.ObjectId;
-    name?: string;
-    userList: UserResult[];
-    messageList: Message[];
+	id?: string;
+	name?: string;
+	userList: UserResult[];
+	messageList: Message[];
 };
 
 export default Chat;
