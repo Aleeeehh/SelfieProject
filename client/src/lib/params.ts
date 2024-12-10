3e67721b854f9286779fdd438711336594538ac6
Aleeeehh <alessioprato03@gmail.com>
2024-12-10 10:50:23 +0100

    modificato server.ts e server_api per deploy

--- client/src/lib/params.ts
+++ client/src/lib/params.ts
@@ -1,7 +1,7 @@
 export const SERVER_PORT = 8000;
-export const SERVER_API = `http://192.168.1.43:${SERVER_PORT}/api`; //per telefono Ale e pc
+//export const SERVER_API = `http://192.168.1.43:${SERVER_PORT}/api`; //per telefono Ale e pc
 //export const SERVER_API = `http://localhost:${SERVER_PORT}/api`; //per pc
-//export const SERVER_API = `http://site232402.tw.cs.unibo.it/api`
+export const SERVER_API = `http://site232402.tw.cs.unibo.it/api`
 //export const SERVER_API = `http://172.20.10.6:${SERVER_PORT}/api`; //per telefono Andre e pc
 
 
